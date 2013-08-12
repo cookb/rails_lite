548d6767ff845b2caaf380f6967b8d0bb3f839ac
cookb <cookb@mit.edu>
2013-08-12 14:19:06 -0700

    Starting Phase III

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -1,23 +1,34 @@
 require 'erb'
+require 'active_support/inflector'
 require_relative 'params'
 require_relative 'session'
 
 class ControllerBase
   attr_reader :params
 
-  def initialize(req, res, route_params)
+  def initialize(req, res, route_params = {})
+    @already_built = false
+    @req = req
+    @res = res
   end
 
   def session
   end
 
   def already_rendered?
+    @already_built
   end
 
   def redirect_to(url)
+    @already_built = true
+    @res.status = 302
+    @res.header["location"] = url
   end
 
-  def render_content(content, type)
+  def render_content(body, content_type)
+    @already_built = true
+    @res.body = body
+    @res.content_type = content_type 
   end
 
   def render(template_name)
--- test/00_server.rb
+++ test/00_server.rb
@@ -12,9 +12,12 @@ trap('INT') { server.shutdown }
 class MyController < ControllerBase
   def go
     render_content("hello world!", "text/html")
-
+    
+    # uncomment to test redirect_to
+    #redirect_to("http://www.google.com")
+    
     # after you have template rendering, uncomment:
-#    render :show
+    #render :show
 
     # after you have sessions going, uncomment:
 #    session["count"] ||= 0
@@ -28,3 +31,4 @@ server.mount_proc '/' do |req, res|
 end
 
 server.start
+
